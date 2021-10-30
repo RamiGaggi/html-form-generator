bc0174dc1e9ebb04ae35b0be03959c0f15c59e71
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-10-30 17:13:42 +0300

    test: add tests for tag

--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -5,7 +5,35 @@ RSpec.describe HexletCode do
     expect(HexletCode::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe HexletCode::Tag do
+    context "with given self-closing tag" do
+      it "returns self-closing tag" do
+        expect(described_class.build("br")).to eq("<br>")
+      end
+    end
+
+    context "with given self-closing tag with attributes" do
+      it "returns self-closing tag with attributes" do
+        expect(described_class.build("img", src: "path/to/image")).to eq(%(<img src="path/to/image">))
+      end
+    end
+
+    context "with given pair-tag" do
+      it "returns pair-tag" do
+        expect(described_class.build("div")).to eq(%(<div></div>))
+      end
+    end
+
+    context "with given pair-tag and attributes" do
+      it "returns pair-tag with attributes" do
+        expect(described_class.build("div", style: "color:red;")).to eq(%(<div style="color:red;"></div>))
+      end
+    end
+
+    context "with given pair-tag, attributes, and body" do
+      it "returns pair-tag with attributes and text" do
+        expect(described_class.build("label", for: "email") { "Email" }).to eq(%(<label for=\"email\">Email</label>))
+      end
+    end
   end
 end
