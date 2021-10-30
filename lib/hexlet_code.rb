f7a24cab1764c20c5e35204061ec2295934872f3
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-10-30 17:14:37 +0300

    feat: add module for tag generation

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,12 +4,23 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   # Generates HTML tag
-  class Tag
+  module Tag
+    SELF_CLOSING = %w[
+      area base br col
+      command embed hr img
+      input keygen link meta
+      param source track wbr
+    ].freeze
+
     def self.build(tag, **attributes)
-      "#{tag}   #{attributes}"
+      attributes = ([""] << attributes.map { |key, el| "#{key}=\"#{el}\"" }).join " "
+      start_tag = ["<", tag]
+      first_tag = attributes == " " ? (start_tag << ">").join : (start_tag << attributes << ">").join
+      if SELF_CLOSING.include?(tag)
+        first_tag
+      else
+        block_given? ? "#{first_tag}#{yield}</#{tag}>" : "#{first_tag}</#{tag}>" # yield if block_given? <- Linter!
+      end
     end
   end
 end
-
-puts HexletCode::Tag.build("img", src: "path/to/image")
-
