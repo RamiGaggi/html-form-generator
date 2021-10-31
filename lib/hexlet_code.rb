51e61aea0bf0f19d9f085dc226db3f3d0f14883d
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-10-31 21:38:00 +0300

    feat: add form generation

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,25 +2,68 @@
 
 require_relative "hexlet_code/version"
 
+# Generates HTML form
 module HexletCode
-  # Generates HTML tag
-  module Tag
+  # Check for self-closing tags
+  module SelfClosingTag
     SELF_CLOSING = %w[
       area base br col
       command embed hr img
       input keygen link meta
       param source track wbr
     ].freeze
+    def self_closing?(tag)
+      SELF_CLOSING.include?(tag)
+    end
+
+    def build_tag(tag, attributes)
+      incomplete_tag = ["<", tag]
+      attributes == " " ? (incomplete_tag << ">").join : (incomplete_tag << attributes << ">").join
+    end
+  end
+
+  # Generates HTML tag
+  class Tag
+    extend SelfClosingTag
 
     def self.build(tag, **attributes)
-      attributes = ([""] << attributes.map { |key, el| "#{key}=\"#{el}\"" }).join " "
-      start_tag = ["<", tag]
-      first_tag = attributes == " " ? (start_tag << ">").join : (start_tag << attributes << ">").join
-      if SELF_CLOSING.include?(tag)
+      attributes = ([""] << attributes.map { |key, el| "#{key}=\"#{el}\"" }).join(" ")
+      first_tag = build_tag(tag, attributes)
+      if self_closing?(tag)
         first_tag
       else
-        block_given? ? "#{first_tag}#{yield}</#{tag}>" : "#{first_tag}</#{tag}>" # yield if block_given? <- Linter!
+        "#{first_tag}#{yield if block_given?}</#{tag}>"
       end
     end
+  end
+
+  # Content between form tags <form>...</form>
+  class UserFormBody
+    def initialize(user)
+      @user = user
+      @tags = []
+    end
+
+    def input(attribute, as: :input) # <- Check this!!!
+      @tags << if as == :input
+                 Tag.build("input", name: attribute, type: "text", value: @user.public_send(attribute))
+               else
+                 Tag.build("textarea", cols: 20, rows: 40, name: attribute) { @user.public_send(attribute) }
+               end
+    end
+
+    def generate
+      @tags.map { |el| "  #{el}" }.join("\n")
+    end
+
+    def empty?
+      @tags.empty?
+    end
+  end
+
+  def self.form_for(user, url: "#")
+    user_form = UserFormBody.new(user)
+    yield user_form
+    Tag.build("form", action: url, method: "post") { user_form.empty? ? "" : "\n#{user_form.generate}\n" }
   end
 end
